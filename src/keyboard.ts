eaf895fae54ad52b4f7279d2d4c00fd5970f6b80
cmedina-dev <christian.medina.atx@gmail.com>
2023-12-21 11:56:29 -0800

    Remove redundant key check for keyboards

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -2,12 +2,9 @@ let isDown = false;
 
 export function checkInput(e: KeyboardEvent): void {
   if (!isDown) {
-    if (e.key === ' ' || e.key === 'Enter') {
-      e.preventDefault;
-      const eTarget = e.target as HTMLElement;
-      eTarget.click();
-      isDown = true;
-    }
+    const eTarget = e.target as HTMLElement;
+    eTarget.click();
+    isDown = true;
   }
 }
 
